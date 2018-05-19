3263dcd7498963c1e3a6d282d23976450526d3fd
Risa Piper <risapiper@208-73-101-105.fttp.usinternet.com>
2018-05-19 15:35:22 -0500

    Update with keys

---
+++ keys.js
@@ -0,0 +1,34 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.TnEwJqxVl4EWXx6SvdOPFgjk8,
+  consumer_secret: process.env.ZuPxSm5c69iHoJiV36VYoE8iMnBczYjnnNWlt1SgIViPBFPgv3,
+  access_token_key: process.env.997570987024515072-usstkEYmME7MKSL1qdsasmGqrwdyCbL,
+  access_token_secret: process.env.tJUuUmF8gVx3ViM10oQ2lKyM3b3NnPv6L9z7vDqde3dna
+
+};
+
+exports.spotify = {
+  id: process.env.a1cfc146535a41b39eeffa66f2ac2337,
+  secret: process.env.10e0dee4ceae42a9ac0372b154ada6b4
+};
+
+
+
+
+
+
+
+// console.log('this is loaded');
+
+// exports.twitter = {
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// };
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
